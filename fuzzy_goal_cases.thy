4e45ef05d232a4ca73a5b923decf4fc3c63dc1cb
Peter Zeller <p_zeller@cs.uni-kl.de>
2020-03-02 11:40:45 +0100

    handling of bound vars

--- fuzzy_goal_cases.thy
+++ fuzzy_goal_cases.thy
@@ -24,8 +24,20 @@ proof (intro impI, fuzzy_goal_cases X )
   have "x = 42" using X.x_def .
   have "blub y" using X.blub .
 
-  show ?case
-    oops
+  oops
+
+  text "The following test is for bound variables with De Bruijn indices."
+
+lemma "(\<exists>blub f. blub = f 42 \<and> f = id \<and> (\<lambda>x y z. y) = g \<and> P blub) \<longrightarrow> C"
+proof (intro impI, elim exE conjE, fuzzy_goal_cases X)
+  case (X blub f)
+
+  have "P blub" using X.P .
+  have "f = id" using X.f_eq .
+  have "blub = f 42" using X.blub_eq .
+  have "(\<lambda>x y z. y) = g" using X.y_eq .
+
+  oops
 
 
 end
